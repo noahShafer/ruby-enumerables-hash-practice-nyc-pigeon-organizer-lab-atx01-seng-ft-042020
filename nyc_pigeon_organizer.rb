3c0ab1f6c44113f2d51ebb19f60a895db577c764
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-07 17:30:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce({}) do |memo, (key, value)| 
+    value.each {|k, v| 
+      v.each {|e| 
+        memo[e] = memo[e] != nil ? memo[e] : {}
+        memo[e][key] = k.to_s
+      }
+    }
+    memo
+  end
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+p nyc_pigeon_organizer(pigeon_data)
