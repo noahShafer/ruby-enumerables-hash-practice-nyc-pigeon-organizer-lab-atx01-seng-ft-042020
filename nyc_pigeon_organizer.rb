a9b68e1313f44d0b5d86464993429d600ac15af4
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-07 17:35:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
       v.each {|e| 
         memo[e] = memo[e] != nil ? memo[e] : {}
         memo[e][key] = memo[e][key] != nil ? memo[e][key] : []
-        memo[e][key].append(k)
+        memo[e][key].append(k.to_s)
       }
     }
     memo
