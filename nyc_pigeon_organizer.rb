68b311a3dde5d4d3732ab7377c2a65b153b8d44a
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-07 17:36:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce({}) do |memo, (key, value)| 
+    value.each {|k, v| 
+      v.each {|e| 
+        memo[e] = memo[e] != nil ? memo[e] : {}
+        memo[e][key] = memo[e][key] != nil ? memo[e][key] : []
+        memo[e][key].append(k.to_s)
+      }
+    }
+    memo
+  end
 end
